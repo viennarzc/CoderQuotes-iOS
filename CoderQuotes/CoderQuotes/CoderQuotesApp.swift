4946a1a62982cae7c7e9255f155671b59dc31f2a
Viennarz Curtiz <viennarzc@gmail.com>
2022-01-25 22:03:20 +0800

    Create Project

---
+++ CoderQuotes/CoderQuotes/CoderQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoderQuotesApp.swift
+//  CoderQuotes
+//
+//  Created by Viennarz Curtiz on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct CoderQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
